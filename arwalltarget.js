195fa21e64a3168ef8c82d727a993fdb15473771
f1th4p <f1th4p@gmail.com>
2024-10-17 16:58:40 +0200

    a

--- arwalltarget.js
+++ arwalltarget.js
@@ -5,6 +5,7 @@
 
 document.addEventListener('DOMContentLoaded', function() {
     var links = document.querySelectorAll('a');
+    debugger;
     links.forEach(function(link) {
         if (link.href.includes('arwall.ar-range.app')) {
             link.setAttribute('target', '_blank');
