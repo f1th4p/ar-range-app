bd3c46f5fece01d799309045f6fa1f8a19a65374
f1th4p <f1th4p@gmail.com>
2024-10-18 09:50:36 +0200

    after refactor

--- custom-code.js
+++ custom-code.js
@@ -1,200 +1,111 @@
-document.addEventListener('DOMContentLoaded', function () {
-    document.querySelectorAll('a[href*="arwall.ar-range.app"]').forEach(function (link) {
-        link.setAttribute('target', '_blank');
+$(document).ready(function () {
+    const addCenterClass = (event) => {
+        const items = $(event.target).find('.owl-item');
+        const centerIndex = Math.floor(items.length / 2);
+        items.removeClass('center');
+        items.eq(event.item.index).addClass('center');
+    };
+
+    const config = {
+        autoplay: true,
+        autoplayTimeout: 4000,
+        autoplayHoverPause: true,
+        margin: 10,
+        nav: true,
+        loop: true,
+        animateIn: 'fadeIn',
+        animateOut: 'fadeOut',
+        responsive: {
+            0: { items: 1 },
+            768: { items: 2 },
+            1000: { items: 3 },
+        },
+        onInitialized: addCenterClass,
+        onTranslated: addCenterClass,
+    };
+
+    const initOwlCarousel = (selector, config) => {
+        $(selector).owlCarousel({ ...config });
+    };
+
+    initOwlCarousel('.projects-col-list.w-dyn-items', config);
+    initOwlCarousel('.projects-col-list-2.w-dyn-items', {
+        ...config,
+        responsive: {
+            0: { items: 1 },
+            600: { items: 2 },
+            1000: { items: 4 },
+        },
+    });
+
+    initOwlCarousel('.recipe-col-list.w-dyn-items', {
+        autoplay: true,
+        autoplayTimeout: 2000,
+        autoplayHoverPause: true,
+        margin: 0,
+        nav: false,
+        loop: true,
+        animateIn: true,
+        responsive: {
+            0: { items: 10 },
+            600: { items: 10 },
+            1000: { items: 10 },
+            1500: { items: 10 },
+        },
+        onInitialized: addCenterClass,
+        onTranslated: addCenterClass,
+    });
+
+    const initSplide = (selector, options) => {
+        $(selector).each(function () {
+            new Splide(this, options).mount();
+        });
+    };
+
+    initSplide('.slider1', {
+        autoplay: true,
+        perPage: 3,
+        perMove: 1,
+        focus: 0, // 0 = left and 'center' = center
+        type: 'loop', // 'loop' or 'slide'
+        gap: '2em', // space between slides
+        arrows: false, // 'slider' or false
+        pagination: false, // 'slider' or false
+        speed: 600, // transition speed in miliseconds
+        dragAngleThreshold: 60, // default is 30
+        autoWidth: false, // for cards with differing widths
+        rewind: false, // go back to beginning when reach end
+        rewindSpeed: 400,
+        waitForTransition: false,
+        updateOnMove: true,
+        trimSpace: false, // true removes empty space from end of list
+        breakpoints: {
+            991: { perPage: 5, gap: '3vw' },
+            767: { perPage: 5, gap: '3vw' },
+            479: { perPage: 5, gap: '3vw' },
+        },
+    });
+
+    initSplide('.slider2', {
+        easing: 'ease',
+        pauseOnFocus: false,
+        autoplay: true,
+        interval: 1500,
+        perPage: 6,
+        perMove: 1,
+        focus: 'center', // 0 = left and 'center' = center
+        type: 'loop', // 'loop' or 'slide'
+        gap: '1em', // space between slides
+        arrows: false, // 'slider' or false
+        pagination: false, // 'slider' or false
+        speed: 1000, // transition speed in miliseconds
+        dragAngleThreshold: 80, // default is 30
+        autoWidth: true, // for cards with differing widths
+        rewind: false, // go back to beginning when reach end
+        rewindSpeed: 0,
+        waitForTransition: false,
+        updateOnMove: true,
+        trimSpace: false,
     });
 });
 
-
-//<script src="https://cdn.jsdelivr.net/gh/f1th4p/ar-range-app@refs/heads/main/arwalltarget.min.js"></script>
-
-//<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
-
-$('.projects-col-list.w-dyn-items').owlCarousel({
-    autoplaySpeed: 2000,
-    slideTransition: 'ease',
-    autoplay: true,
-    autoplayTimeout: 4000,
-    autoplayHoverPause: true,
-    margin: 10,
-    nav: true,
-    loop: true,
-    animateIn: 'fadeIn',
-    animateOut: 'fadeOut',
-    /*navText: [" < div class = 'nav-btn prev-slide' >< /div>", " < div class = 'nav-btn next-slide' >< /div>"],*/
-    items: 1,
-    responsive: {
-        0: {
-            autoplay: false,
-            items: 1
-        },
-        768: {
-            autoplay: true,
-            items: 2
-        },
-        1000: {
-            autoplay: true,
-            items: 3
-        }
-    },
-    onInitialized: addCenterClass,
-    onTranslated: addCenterClass
-});
-$('.projects-col-list-2.w-dyn-items').owlCarousel({
-    autoplaySpeed: 2000,
-    slideTransition: 'ease',
-    autoplay: true,
-    autoplayTimeout: 4000,
-    autoplayHoverPause: true,
-    margin: 10,
-    nav: true,
-    loop: true,
-    animateIn: 'fadeIn',
-    animateOut: 'fadeOut',
-    /*navText: [" < div class = 'nav-btn prev-slide' >< /div>", " < div class = 'nav-btn next-slide' >< /div>"],*/
-    /*navText: [""],*/
-    items: 1,
-    responsive: {
-        0: {
-            autoplay: false,
-            items: 1
-        },
-        600: {
-            autoplay: true,
-            items: 2
-        },
-        1000: {
-            autoplay: true,
-            items: 4
-        }
-    },
-    onInitialized: addCenterClass,
-    onTranslated: addCenterClass
-});
-$('.recipe-col-list.w-dyn-items').owlCarousel({
-    autoplay: true,
-    autoplayTimeout: 2000,
-    autoplayHoverPause: true,
-    margin: 0,
-    nav: false,
-    loop: true,
-    animateIn: true,
-    /*navText: [" < div class = 'nav-btn prev-slide' >< /div>", " < div class = 'nav-btn next-slide' >< /div>"],*/
-    navText: [""],
-    responsive: {
-        0: {
-            items: 10
-        },
-        600: {
-            items: 10
-        },
-        1000: {
-            items: 10
-        },
-        1500: {
-            items: 10
-        }
-    },
-    onInitialized: addCenterClass,
-    onTranslated: addCenterClass
-});
-
-function addCenterClass(event) {
-    var items = $(event.target).find('.owl-item');
-    var centerIndex = Math.floor(items.length / 2);
-    items.removeClass('center');
-    //items.eq(centerIndex).addClass('center');
-    items.eq(event.item.index).addClass('center');
-}
-
-//<script src="https://cdn.jsdelivr.net/npm/@splidejs/splide@3.2.2/dist/js/splide.min.js"></script>
-function slider1() {
-    let splides = $('.slider1');
-    for (let i = 0, splideLength = splides.length; i < splideLength; i++) {
-        new Splide(splides[i], {
-            // Desktop on down
-            autoplay: true,
-            perPage: 3,
-            perMove: 1,
-            focus: 0, // 0 = left and 'center' = center
-            type: 'loop', // 'loop' or 'slide'
-            gap: '2em', // space between slides
-            arrows: false, // 'slider' or false
-            pagination: false, // 'slider' or false
-            speed: 600, // transition speed in miliseconds
-            dragAngleThreshold: 60, // default is 30
-            autoWidth: false, // for cards with differing widths
-            rewind: false, // go back to beginning when reach end
-            rewindSpeed: 400,
-            waitForTransition: false,
-            updateOnMove: true,
-            trimSpace: false, // true removes empty space from end of list
-            breakpoints: {
-                991: {
-                    // Tablet
-                    perPage: 5,
-                    gap: '3vw',
-                },
-                767: {
-                    // Mobile Landscape
-                    perPage: 5,
-                    gap: '3vw',
-                },
-                479: {
-                    // Mobile Portrait
-                    perPage: 5,
-                    gap: '3vw',
-                }
-            }
-        }).mount();
-    }
-}
-slider1();
-
-function slider2() {
-    let splides = $('.slider2');
-    for (let i = 0, splideLength = splides.length; i < splideLength; i++) {
-        new Splide(splides[i], {
-            // Desktop on down
-            easing: 'ease',
-            pauseOnFocus: false,
-            autoplay: true,
-            interval: 1500,
-            perPage: 6,
-            perMove: 1,
-            focus: 'center', // 0 = left and 'center' = center
-            type: 'loop', // 'loop' or 'slide'
-            gap: '1em', // space between slides
-            arrows: false, // 'slider' or false
-            pagination: false, // 'slider' or false
-            speed: 1000, // transition speed in miliseconds
-            dragAngleThreshold: 80, // default is 30
-            autoWidth: true, // for cards with differing widths
-            rewind: false, // go back to beginning when reach end
-            rewindSpeed: 0,
-            waitForTransition: false,
-            updateOnMove: true,
-            trimSpace: false,
-
-            /*breakpoints: {
-                991: {
-                    // Tablet
-                    perPage: 5,
-                    gap: '4vw',
-                },
-                767: {
-                    // Mobile Landscape
-                    perPage: 5,
-                    gap: '4vw',
-                },
-                479: {
-                    // Mobile Portrait
-                    perPage: 5,
-                    gap: '4vw',
-                }
-            }*/
-
-        }).mount();
-    }
-}
-slider2();
-//<script type="text/javascript" src="https://widget.clutch.co/static/js/widget.js"></script>
